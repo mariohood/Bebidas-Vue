08da4c9c3280d1274d868bd8a7bab4d7b129e1ae
mariohood <manhattan199@hotmail.com>
2025-03-10 16:43:13 -0300

    225 Criando o Formulario de Busqueda

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+  import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1>Desde Pinia</h1>
+  <Header />
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="mx-auto container px-5 py-16">
+      <div>
+
+      </div>
+
+
+      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+      >
+        <div class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="ingrediente">Nombre o Ingrediente</label>
+          <input
+            id="ingrediente"
+            type="text"
+            class="p-3 w-full rounded-lg focus:outline-none"
+            placeholder="Nombre o Ingrediente: ej. Vodka, Tequila, etc"
+          >
+        </div>
+
+        <div class="space-y-4">
+          <label
+            class="block text-white uppercase font-extrabold text-lg"
+            for="categoria">Categoría</label>
+          <select
+            id="categoria"
+            class="p-3 w-full rounded-lg focus:outline-none"
+          >
+            <option value="">-- Seleccione --</option>
+          </select>
+        </div>
+
+        <input 
+          type="submit"
+          class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2
+          rounded-lg uppercase"
+          value="Buscar Recetas"
+        />
+      </form>
+
+    </div>
+
+  </header>
+</template>
+
