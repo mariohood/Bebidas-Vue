c0f5735cf0ad0868d3706f982aa8bca6f8bb65bc
mariohood <manhattan199@hotmail.com>
2025-03-10 18:23:50 -0300

    230 Chamando nossa API desde o Store e mostar categorias no formulario

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,21 @@
 <script setup>
-  import { RouterLink } from 'vue-router'
+  import { computed } from 'vue'
+  import { RouterLink, useRoute } from 'vue-router'
+  import { useBebidasStore } from '../stores/bebidas'
+
+  const route = useRoute()
+  const store = useBebidasStore()
+  console.log(store.categorias)
+
+  const paginaInicio = computed(() => route.name === 'inicio')
+
+
 </script>
 
 <template>
   <header
     class="bg-slate-800"
+    :class="{ 'header': paginaInicio }"
     >
     <div class="mx-auto container px-5 py-16">
       <div class="flex justify-between items-center">
@@ -36,8 +47,9 @@
 
       </div>
 
-
-      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+      <form 
+        class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+        v-if="paginaInicio"
       >
         <div class="space-y-4">
           <label
@@ -60,6 +72,11 @@
             class="p-3 w-full rounded-lg focus:outline-none"
           >
             <option value="">-- Seleccione --</option>
+            <option
+              v-for="categoria in store.categorias"
+              :key="categoria.strCategory"
+              :value="categoria.strCategory"
+              >{{ categoria.strCategory }}</option>
           </select>
         </div>
 
@@ -75,4 +92,12 @@
 
   </header>
 </template>
+
+<style>
+  .header {
+    background-image: url('/img/bg.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+</style>
 
