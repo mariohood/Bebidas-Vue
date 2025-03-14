891c774d29ab76b8c6796bb204c662bfd6d65ddb
mariohood <manhattan199@hotmail.com>
2025-03-14 20:57:43 -0300

    255 Reseteando o estado no Pinia

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -2,11 +2,14 @@ import { ref, watch, onMounted, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
 import { useModalStore } from './modal'
+import { useNotificacionStore } from './notificaciones' 
 
 export const useFavoritosStore = defineStore ('favoritos', () => {
 
   const bebidas = useBebidasStore()
   const modal = useModalStore()
+  const notificaciones = useNotificacionStore()
+
   const favoritos = ref([])
 
   onMounted(() => {
@@ -34,6 +37,13 @@ export const useFavoritosStore = defineStore ('favoritos', () => {
 
   function agregarFavorito() {
     favoritos.value.push(bebidas.receta)
+
+    notificaciones.mostrar= true
+    notificaciones.texto = 'Se agregó a favoritos'
+
+    setTimeout(() => {
+      notificaciones.$reset()
+    }, 3000)
   }
 
   function handelClickFavorito() {
